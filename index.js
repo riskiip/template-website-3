2e33c31a675d80c7ccb904d59837865a4aa6e930
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2024-01-31 23:39:00 +0700

    fix: align with requirement

--- index.js
+++ index.js
@@ -28,16 +28,16 @@ let foodMenuList = document.querySelector('.food-item-wrap')
 
 let foodCategory = document.querySelector('.food-category')
 
-let categories = foodCategory.querySelectorAll('button')
+// let categories = foodCategory.querySelectorAll('button')
 
-Array.from(categories).forEach((item, index) => {
-    item.onclick = (e) => {
-        let currCat = foodCategory.querySelector('button.active')
-        currCat.classList.remove('active')
-        e.target.classList.add('active')
-        foodMenuList.classList ='food-item-wrap '+ e.target.getAttribute('data-food-type')
-    }
-})
+// Array.from(categories).forEach((item, index) => {
+//     item.onclick = (e) => {
+//         let currCat = foodCategory.querySelector('button.active')
+//         currCat.classList.remove('active')
+//         e.target.classList.add('active')
+//         foodMenuList.classList ='food-item-wrap '+ e.target.getAttribute('data-food-type')
+//     }
+// })
 
 // on scroll animation
 
